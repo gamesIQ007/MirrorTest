5dbce3b01caa3dc29ad3d332c3a66023da8eea60
gamesIQ007 <bdagames@ya.ru>
2023-06-28 21:30:38 +0300

    41.4. Quick Start

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Mirror;
+
+/// <summary>
+/// Игрок
+/// </summary>
+public class Player : NetworkBehaviour
+{
+    /// <summary>
+    /// Скорость перемещения
+    /// </summary>
+    [SerializeField] private float m_MovementSpeed;
+
+
+    private void Update()
+    {
+        if (isOwned)
+        {
+            Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+            transform.Translate(dir * Time.deltaTime * m_MovementSpeed);
+        }
+    }
+}
